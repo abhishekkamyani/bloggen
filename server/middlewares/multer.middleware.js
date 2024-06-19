8d7e1243324ed7fa5007ccee9f3826615e5f4f49
abhishek kamyani <abhishekkumar.bscsf21kdk@iba-suk.edu.pk>
2024-06-19 22:28:52 +0500

    use fs and path

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -1,19 +1,23 @@
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const destination = path.join(__dirname, "../uploads");
+console.log(destination);
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./uploads")
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        const extension = file.originalname.split('.').pop(); // Get the file extension
-        cb(null, file.originalname + '-' + uniqueSuffix + "." + extension);
-    }
+  destination: function (req, file, cb) {
+    cb(null, destination);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const extension = file.originalname.split(".").pop(); // Get the file extension
+    cb(null, file.originalname + "-" + uniqueSuffix + "." + extension);
+  },
 });
 
-
 const upload = multer({
-    limits: { fieldSize: 100 * 1024 * 1024 }, // Increase the field size limit to 100MB 
-    storage: storage,
+  limits: { fieldSize: 100 * 1024 * 1024 }, // Increase the field size limit to 100MB
+  storage: storage,
 });
 module.exports = upload;
