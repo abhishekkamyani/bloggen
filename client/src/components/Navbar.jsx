69fe169e1b9864e50c540edba58b2d367d8980fa
abhishek kamyani <abhishekkumar.bscsf21kdk@iba-suk.edu.pk>
2024-07-14 14:18:07 +0500

    stored images of avatar and cover in the server directory

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
 
   useEffect(() => {
     initTWE({ Collapse, Dropdown });
-  }, []);
+  });
 
   return (
     <>
--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
 
-app.use(express.static("dist"));
-// app.use("/*", express.static("dist"));
+app.use(express.static("public"));
+// app.use("/*", express.static("public"));
 
 app
     .use("/api/auth", authRouter)
